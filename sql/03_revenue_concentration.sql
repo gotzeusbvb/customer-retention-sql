d5da557a1c5f0df8e0ace537e30a16c1a440581a
AEMC <160779799+gotzeusbvb@users.noreply.github.com>
2026-01-19 17:08:02 -0500

    Update 03_revenue_concentration.sql

--- sql/03_revenue_concentration.sql
+++ sql/03_revenue_concentration.sql
@@ -2,3 +2,55 @@
 -- Analyzes revenue distribution using customer ranking and deciles
 -- Identifies top revenue-contributing customers
 -- Database: RetailAnalyticsDW
+
+USE RetailAnalyticsDW;
+GO
+
+WITH customer_revenueAS (
+SELECT
+        CustomerKey,
+SUM(NetSalesAmount)AS TotalRevenue
+FROM dbo.FactSales
+GROUPBY CustomerKey
+),
+rankedAS (
+SELECT
+        CustomerKey,
+        TotalRevenue,
+RANK()OVER (ORDERBY TotalRevenueDESC)AS RevenueRank,
+COUNT(*)OVER ()AS TotalCustomers
+FROM customer_revenue
+)
+SELECT
+*,
+CAST(1.0* RevenueRank/ TotalCustomersASdecimal(6,4))AS CustomerPercentile
+FROM ranked
+ORDERBY RevenueRank;
+
+--- Top 10% ---
+
+USE RetailAnalyticsDW;
+GO
+
+WITH customer_revenueAS (
+SELECT
+        CustomerKey,
+SUM(NetSalesAmount)AS TotalRevenue
+FROM dbo.FactSales
+GROUPBY CustomerKey
+),
+rankedAS (
+SELECT
+        CustomerKey,
+        TotalRevenue,
+NTILE(10)OVER (ORDERBY TotalRevenueDESC)AS RevenueDecile
+FROM customer_revenue
+)
+SELECT
+    RevenueDecile,
+COUNT(*)AS Customers,
+SUM(TotalRevenue)AS Revenue
+FROM ranked
+GROUPBY RevenueDecile
+ORDERBY RevenueDecile;
+
